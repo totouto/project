a725738e9826544d541ac9356df75f6bc7438784
totouto <57044456+totouto@users.noreply.github.com>
2020-07-22 14:04:30 +0900

    Add files via upload

---
+++ KernelGAN/KernelGAN_main.py
@@ -0,0 +1,60 @@
+import os
+import tqdm
+
+from KernelGAN.configs import Config
+from KernelGAN.data import DataGenerator
+from KernelGAN.kernelGAN import KernelGAN
+from KernelGAN.learner import Learner
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QFileDialog, QMainWindow, QApplication
+from PyQt5.QtGui import QPixmap
+from PyQt5 import uic
+
+
+def train(conf, QMainWindow):
+    gan = KernelGAN(conf, QMainWindow)
+    learner = Learner()
+    data = DataGenerator(conf, gan)
+    QMainWindow.label_status.setText("During KernelGAN Estimation")
+    for iteration in tqdm.tqdm(range(conf.max_iters), ncols=60):
+        [g_in, d_in] = data.__getitem__(iteration)
+        gan.train(g_in, d_in)
+        learner.update(iteration, gan)
+        QMainWindow.progressBar.setValue(iteration / conf.max_iters * 100 + 1)
+    gan.finish(QMainWindow)
+
+
+def main():
+    """The main function - performs kernel estimation (+ ZSSR) for all images in the 'test_images' folder"""
+    import argparse
+    # Parse the command line arguments
+    prog = argparse.ArgumentParser()
+    prog.add_argument('--input-dir', '-i', type=str, default='test_images/lr_x2', help='path to image input directory.')
+    prog.add_argument('--output-dir', '-o', type=str, default='results', help='path to image output directory.')
+    prog.add_argument('--X4', action='store_true', help='The wanted SR scale factor')
+    prog.add_argument('--SR', action='store_true', help='when activated - ZSSR is not performed')
+    prog.add_argument('--real', action='store_true', help='ZSSRs configuration is for real images')
+    prog.add_argument('--noise_scale', type=float, default=1., help='ZSSR uses this to partially de-noise images')
+    args = prog.parse_args()
+    # Run the KernelGAN sequentially on all images in the input directory
+    for filename in os.listdir(os.path.abspath(args.input_dir)):
+        conf = Config().parse(create_params(filename, args))
+        train(conf)
+    prog.exit(0)
+
+
+def create_params(filename, args):
+    params = ['--input_image_path', os.path.join(args.input_dir, filename),
+              '--output_dir_path', os.path.abspath(args.output_dir),
+              '--noise_scale', str(args.noise_scale)]
+    if args.X4:
+        params.append('--X4')
+    if args.SR:
+        params.append('--do_ZSSR')
+    if args.real:
+        params.append('--real_image')
+    return params
+
+
+if __name__ == '__main__':
+    main()
